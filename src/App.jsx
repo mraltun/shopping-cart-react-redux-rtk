47c35e22216a1f573747a71b40a59a315c2171a5
Murat Altun <mraltun@gmail.com>
2022-08-30 14:12:07 +0300

    Added CartContainer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import CartContainer from "./components/CartContainer";
 
 const App = () => {
   return (
     <main>
       <Navbar />
+      <CartContainer />
     </main>
   );
 };
