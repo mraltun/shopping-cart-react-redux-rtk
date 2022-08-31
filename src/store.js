e05dc37c0c6812897fc5c9f88512952519c96286
Murat Altun <mraltun@gmail.com>
2022-08-31 13:01:31 +0300

    Added modalReducer to store

--- src/store.js
+++ src/store.js
@@ -1,8 +1,12 @@
-// A better createStore
+// An easier and better createStore
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./features/cart/cartSlice";
+import modalReducer from "./features/modal/modalSlice";
 
 // Combine reducers in a single object. Each reducer controls a slice.
 export const store = configureStore({
-  reducer: { cart: cartReducer },
+  reducer: {
+    cart: cartReducer,
+    modal: modalReducer,
+  },
 });
